e46a90ebdc4f5c9fb166746e241528e3c59b8151
BugHunter-X <askzenai@gmail.com>
2025-02-20 23:28:05 +0100

    Add index-solution.js file

---
+++ index-solution.js
@@ -0,0 +1,14 @@
+```javascript
+//File: index-solution.js
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div className="bg-red-500 p-4">
+      <p>This is some text</p>
+    </div>
+  );
+}
+
+export default MyComponent;
+```
